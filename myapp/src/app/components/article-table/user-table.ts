56d0106f97547ea4ee0453726a51792f26935411
Moaaz <moaaz.bahnassawi@objectmethod.it>
2026-06-18 12:55:05 +0200

    Ciccio come sta andando

--- myapp/src/app/components/article-table/user-table.ts
+++ myapp/src/app/components/article-table/user-table.ts
@@ -1,21 +1,33 @@
 import { LoadingService } from '../../services/loading-service';
 import { UserService } from '../../services/user-service';
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { Component, computed, Signal, signal, SimpleChanges } from '@angular/core';
 import { User } from '../../models/User';
 import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { Observable } from 'rxjs';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-article-table',
-  imports: [CommonModule, TableModule, ButtonModule],
+  imports: [
+    CommonModule,
+    TableModule,
+    ButtonModule,
+    InputTextModule,
+    FloatLabelModule,
+    FormsModule
+  ],
   templateUrl: './article-table.html',
   styleUrl: './article-table.css',
 })
 export class UserTable {
-  utenti: User[] = [];
+  utenti = signal<User[]>([]);
+  utentiOriginali = signal<User[]>([]);
   loadingValue!: Observable<boolean>;
+  username = signal('');
 
   constructor(private userService: UserService, private loadingService: LoadingService) { }
 
@@ -28,19 +40,34 @@ export class UserTable {
     this.getUsers();
   }
 
-  private getUsers(): void {
 
+  onSearch() {
+    this.utenti.set(this.filterUsers());
+    this.username.set('');
+  }
+
+  private getUsers(): void {
     this.userService.getAllUsers().subscribe({
       next: (users: User[]) => {
-        this.utenti = users;
+        this.utentiOriginali.set(users);
+        this.utenti.set(users);
         this.loadingService.updateIsLoadingValue(false);
       },
       error: (err) => {
         this.loadingService.updateIsLoadingValue(false);
       }
     });
-
   }
+
+  private filterUsers() {
+    return this.utentiOriginali().filter(utente => utente.username === this.username());
+  }
+
+
+
+
+
+
 
 
 
